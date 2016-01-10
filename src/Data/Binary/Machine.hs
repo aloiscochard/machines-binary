eb8f4688d7f24fceef32080aa317a12a3d0d50c2
Alois Cochard <alois.cochard@gmail.com>
2016-01-10 19:54:32 +0100

    Remove stale `Get/Put` module and keep errors in structures.

--- src/Data/Binary/Machine.hs
+++ src/Data/Binary/Machine.hs
@@ -7,23 +7,22 @@ import Data.Machine (MachineT(..), ProcessT, Step(Await, Yield), Is(Refl), auto,
 
 import qualified Data.ByteString.Lazy as Lazy
 
-streamGet :: Monad m => Get a -> ProcessT m ByteString a
+streamGet :: Monad m => Get a -> ProcessT m ByteString (Either String a)
 streamGet getA = processDecoder' (runGetIncremental getA) (streamGet getA)
 
-processGet :: Monad m => Get a -> ProcessT m ByteString a
+processGet :: Monad m => Get a -> ProcessT m ByteString (Either String a)
 processGet getA = processDecoder (runGetIncremental getA)
 
-processDecoder :: Monad m => Decoder a -> ProcessT m ByteString a
+processDecoder :: Monad m => Decoder a -> ProcessT m ByteString (Either String a)
 processDecoder decA = processDecoder' decA stopped
 
-processDecoder' :: Monad m => Decoder a -> ProcessT m ByteString a -> ProcessT m ByteString a
+processDecoder' :: Monad m => Decoder a -> ProcessT m ByteString (Either String a) -> ProcessT m ByteString (Either String a)
 processDecoder' decA r = MachineT . return $ Await f Refl stopped where
   f xs = case pushChunk decA xs of
-    -- TODO Add `Fail` case
-    Fail _ _ s    -> error s
-    Done _ _ a    -> MachineT . return $ Yield a r
+    Fail _ _ e    -> yield' $ Left e
+    Done _ _ a    -> yield' $ Right a
     decA'         -> processDecoder' decA' r
-    -- decA'         -> error "foo"
+  yield' ea = MachineT . return $ Yield ea r
 
 processPut :: Monad m => (a -> Put) -> ProcessT m a ByteString
 processPut f = auto $ Lazy.toStrict . runPut . f
--- src/Data/Binary/Put/Machine.hs
+++
@@ -1,8 +0,0 @@
-module Data.Binary.Put.Machine where
-
-import Data.ByteString.Lazy (ByteString)
-import Data.Binary.Put (Put, runPut)
-import Data.Machine
-
-processPut :: Monad m => (a -> Put) -> ProcessT m a ByteString
-processPut f = repeatedly $ await >>= g where g = yield . runPut . f
